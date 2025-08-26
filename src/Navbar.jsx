b57902ed5dcc20210b62d4a7edb73553b98581b9
Daniel Liu <143345383+d4n1elliu@users.noreply.github.com>
2025-08-27 00:36:31 +1000

    Fixed up comments in Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="bg-blue-800 text-white p-4 flex justify-between items-center fixed top-0 left-0 right-0 w-full h-16 shadow-md z-50">
+        <nav className="bg-blue-800 text-white px-6 py-4 flex justify-between items-center fixed top-0 left-0 right-0 w-full h-16 shadow-md z-50">
             {/* Smaller EduMap in Navbar*/}
             <h1 className="text-2xl font-bold">EduMap</h1>
 
-            {/* White text inside of the links*/}
+            {/* White nav links*/}
             <ul className="flex space-x-6 text-white font-medium">
                 <li><a href="#home" className="hover:text-orange-400">Course Questionnaire</a></li>
                 <li><a href="#features" className="hover:text-orange-400">Job Prospects</a></li>
