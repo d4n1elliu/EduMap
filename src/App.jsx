4a03825dada7b274a3098dd4cc6c264aa47730d4
Daniel Liu <143345383+d4n1elliu@users.noreply.github.com>
2025-08-27 00:05:27 +1000

    Navbar and font changes
    Added a navigation bar on the home screen and enlarged the font of the website title

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-orange-500 8xl text-8xl font-extrabold">EduMap</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,9 +19,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
