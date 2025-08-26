a10bf6dd13837746bc7d79c5898c7ff735c67d7e
Daniel Liu <daniel.liu8750@gmail.com>
2025-08-26 13:58:46 +1000

    Uninstalled tailwindcss newest version
    Replaced the latest version of tailwindcss with tailwindcss v3.4x

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-orange-500 text-3xl font bold">EduMap</h1>
+        <h1 className="text-orange-500 font bold">EduMap</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
