b2efa3319ffd3a9be2e4e0902b0ae9b25a62c30e
Daniel Liu <143345383+d4n1elliu@users.noreply.github.com>
2025-08-31 13:22:29 +1000

    Updated Navbar.jsx
    - Changed hover:text-colour-orange-400 to hover:text-colour-blue-400
    - Changed text-blue-300 to text-blue-600 for EduMap title

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,26 +5,26 @@ function Navbar() {
     return (
         <nav className="bg-orange-500 text-white px-6 py-5 flex justify-between items-center fixed top-0 left-0 right-0 w-full h-24 shadow-md z-50">
 
-            <h1 className="text-blue-300 text-3xl font-bold">
+            <h1 className="text-blue-600 text-3xl font-bold">
                 <NavLink to="/">EduMap</NavLink>
             </h1>
 
             <ul className="flex space-x-6 text-white font-medium">
                 <li>
                     <NavLink to="/course-questionnaire"
-                        className={({ isActive }) => isActive ? "text-orange-400" : "hover:text-orange-400"}>
+                        className={({ isActive }) => isActive ? "text-orange-400" : "hover:text-blue-400"}>
                         Course Questionnaire
                     </NavLink>
                     </li>
                     <li>
                     <NavLink to="/job-prospects"
-                        className={({ isActive }) => isActive ? "text-orange-400" : "hover:text-orange-400"}>
+                        className={({ isActive }) => isActive ? "text-orange-400" : "hover:text-blue-400"}>
                         Job Prospects
                     </NavLink>
                     </li>
                     <li>
                     <NavLink to="/buddy-system"
-                        className={({ isActive }) => isActive ? "text-orange-400" : "hover:text-orange-400"}>
+                        className={({ isActive }) => isActive ? "text-orange-400" : "hover:text-blue-400"}>
                         Buddy Program
                     </NavLink>
                 </li>
