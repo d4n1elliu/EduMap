785c1dc92c2bd96d7f27d31911211eb4ff94d137
Daniel Liu <daniel.liu8750@gmail.com>
2025-09-08 01:04:50 +1000

    Updated auth.jsx
    Added comments for auth.jsx

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,10 +2,12 @@ import api from "./api";
 
 const controller = 'auth';
 
+// Temporary student number for login
 export const login = async (username, password) => {
     return await api.post(`${controller}/login`, { username, password });
 };
 
+// Temporary student number for registration
 export const register = async (email, username, password) => {
     return await api.post(`${controller}/register`, { email, username, password, studentNumber });
 };
