502ee4880dc1270d20ce2e23b38df7d1f234ef56
Daniel Liu <143345383+d4n1elliu@users.noreply.github.com>
2025-09-25 16:20:11 +1000

    Added EduMap logo to website
    EduMap logo has been added to the top left for the navigation bar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom"; 
+import { NavLink } from "react-router-dom";
+import EduMapLogo from "./assets/EduMap Logo 300dpi.png"; 
 
 function Navbar() {
     return (
         <nav className="bg-gradient-to-r from-slate-600 via-slate-500 via-orange-300 to-orange-400 text-white px-6 py-5 flex justify-between items-center fixed top-0 left-0 right-0 w-full h-24 shadow-md z-50"> 
         {/* Navbar Background Styling */}
 
-            <h1 className="text-blue-400 text-3xl font-bold">
-                <NavLink to="/">EduMap</NavLink>
-            </h1>
+            <div className="flex items-center space-x-3">
+                <img 
+                    src={EduMapLogo} 
+                    alt="EduMap Logo" 
+                    className="h-15 w-auto object-contain"
+                />
+                <h1 className="text-blue-400 text-2xl font-bold">
+                    <NavLink to="/">EduMap</NavLink>
+                </h1>
+            </div>
 
             {/* Login & SignUp button */}
             <ul className="flex space-x-10 text-white font-medium">
