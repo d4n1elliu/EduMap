1706ea8c1f3f8412471caa1ded20b7e174dd2366
Daniel Liu <143345383+d4n1elliu@users.noreply.github.com>
2025-09-25 17:22:32 +1000

    feat(navbar): Added clarification comments, implemented standardised link style
    Summary
    - Cleaned up navbar UI and code comments for better readability and consistency

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,8 +12,8 @@ function Navbar() {
         window.location.reload();
     };
     return (
-        <nav className="bg-gradient-to-r from-slate-600 via-slate-500 via-orange-300 to-orange-400 text-white px-6 py-5 flex justify-between items-center fixed top-0 left-0 right-0 w-full h-24 shadow-md z-50"> 
-        {/* Navbar Background Styling */}
+        <nav className="bg-gradient-to-r from-slate-600 via-slate-500 via-orange-300 to-orange-400 text-white px-6 py-5 flex justify-between items-center fixed top-0 left-0 right-0 w-full h-24 z-50"> 
+        {/* Main navigation <div> container: fixed at the top, gradient background*/}
 
             <div className="flex items-center space-x-3">
                 <img 
@@ -26,26 +26,28 @@ function Navbar() {
                 </h1>
             </div>
 
-            {/* Navigation bar appears once user logs in/signs up*/}
+            {/* Navigation bar appears when User IS LOGGED/SIGNED IN Functionality*/}
             {token ? (
                 <ul className="flex items-center space-x-8 text-white font-medium">
                     <li>
-                        <NavLink to="/course-questionnaire" className={({ isActive }) => isActive ? "text-blue-700" : "hover:text-blue-200"}>Course Questionnaire</NavLink>
+                        <NavLink to="/course-questionnaire" className={({ isActive }) => isActive ? "text-green-500" : "hover:text-blue-700"}>Course Questionnaire</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/job-prospects" className={({ isActive }) => isActive ? "text-blue-700" : "hover:text-blue-200"}>Job Prospects</NavLink>
+                        <NavLink to="/job-prospects" className={({ isActive }) => isActive ? "text-green-500" : "hover:text-blue-700"}>Job Prospects</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/buddy-system" className={({ isActive }) => isActive ? "text-blue-700" : "hover:text-blue-200"}>Buddy System</NavLink>
+                        <NavLink to="/buddy-system" className={({ isActive }) => isActive ? "text-green-500" : "hover:text-blue-700"}>Buddy System</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/about" className={({ isActive }) => isActive ? "text-blue-700" : "hover:text-blue-200"}>About Us</NavLink>
+                        <NavLink to="/about" className={({ isActive }) => isActive ? "text-green-500" : "hover:text-blue-700"}>About Us</NavLink>
                     </li>
                     <li>
                         <button onClick={handleLogout} className="ml-5 px-4 py-1 rounded bg-white/20 hover:bg-white/20">Logout</button>
                     </li>
                 </ul>
             ) : (
+
+                /* Login & Sign up Menu are visible when users are NOT LOGGED/SIGNED IN Functionality*/
                 <ul className="flex space-x-10 text-white font-medium">
                     <li>
                         <NavLink to="/signup"
