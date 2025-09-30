8583d3674c8dbee9387062b34bdd495677462bcd
Daniel Liu <daniel.liu8750@gmail.com>
2025-09-30 15:31:06 +1000

    Updated page name for Events & Networking Map

--- src/pages/EventsAndNetworkingMap.jsx
+++ src/pages/EventsAndNetworkingMap.jsx
@@ -2,14 +2,14 @@ import Background from './Background';
 import Footer from './Footer';
 
 
-export default function JobProspects() {
+export default function EventsAndNetworkingMap() {
     return (
-        // JSX for the Job Prospects page
+        // JSX for the Events & Networking Map page
         <Background>
             <div className="w-full max-w-6xl px-6 pt-28 mx-auto flex-1">
                 <main className="flex-1">
                     <section className="text-center mb-10">
-                        <h1 className="text-5xl md:text-6xl font-extrabold text-orange-500 mb-3">Job Prospects</h1>
+                        <h1 className="text-5xl md:text-6xl font-extrabold text-orange-500 mb-3">Events & Networking Map</h1>
                         <p className="text-lg md:text-xl font-bold text-black-700 max-w-3xl mx-auto">Explore career paths, compare your skills to market needs and follow a step-by-step roadmap with mentor support.</p>
                     </section>
 
